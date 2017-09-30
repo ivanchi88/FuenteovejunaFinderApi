370312b892c11feaf63af47e5095ff606f792519
diego <frikiman34@hotmail.com>
2017-09-30 23:12:06 +0200

    express

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('hello!'));
+
+app.listen(8000, (err) => {
+  if(err) {
+    console.error('there was an error while creating the server');
+    process.exit(1);
+  }
+  console.log('Listening on 8000');
+});
